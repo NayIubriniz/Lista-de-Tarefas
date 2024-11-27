c679e85e7dc6993bf70097d91f280a0fa6935332
Nayra Rodrigues <nayra_iubriniz@outlook.com>
2024-11-26 21:26:48 -0300

    subindo meu projeto

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    conn = sqlite3.connect("tasks.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task TEXT NOT NULL,
+        done BOOLEAN NOT NULL DEFAULT 0)
+""")
+    conn.commit()
+    conn.close()
+
+
+init_db()
+
+
+@app.route("/")
+def index():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, task, done FROM tasks")
+    tasks = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route("/add", methods=['POST'])
+def add_task():
+    task = request.form.get("task")
+    if task:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO tasks (task, done) VALUES (?, ?)",
+                       (task, False))
+        conn.commit()
+        conn.close()
+        return redirect(url_for("index"))
+
+
+@app.route("/delete/<int:task_id>")
+def delete_task(task_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
+
+
+@app.route("/complete/<int:task_id>")
+def complete_task(task_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE tasks SET done = ? WHERE id = ?", (1, task_id))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
